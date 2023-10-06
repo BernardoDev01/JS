2cec3ac4136271f836babfd7a28255947e93467b
BernardoValle <dev.bernardovalle@gmail.com>
2023-10-06 20:14:40 -0300

    let

---
+++ Variavel/003/var003.js
@@ -0,0 +1,11 @@
+let txt = 'É bom treinar para pegar a prática.'
+let tx = 'Sempre bom aprender coisas novas'
+let t = 'Bem-vindo!'
+let num = 12.5
+let n = 12
+
+alert(t)
+document.write(txt)
+document.write(tx)
+console.log(num)
+document.write(`Número de tentativas ${n} ou não`)
