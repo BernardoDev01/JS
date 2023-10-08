509825e9831b43b60923a87b1c7b4cf08c8d5b2f
BernardoValle <dev.bernardovalle@gmail.com>
2023-10-08 20:24:42 -0300

    var004

---
+++ variavel/004/var003.js
@@ -0,0 +1,9 @@
+var teste = 'Primeiro valor'
+var teste02 = 'Segundo valor'
+console.log(teste)
+console.log(teste02)
+document.write(teste)
+document.write(teste02)
+var teste = 'Valor trocado'
+document.write(teste)
+console.log(teste)
