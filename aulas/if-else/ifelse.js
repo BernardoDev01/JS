cd2aab4ef01f118da7b35a4e78c0cdc66084413d
BernardoValle <dev.bernardovalle@gmail.com>
2023-10-09 23:07:10 -0300

    if e else

---
+++ aulas/if-else/ifelse.js
@@ -0,0 +1,37 @@
+if (2 == 2) {
+    document.write(' Entrou dentro do block if(verdadeiro)')
+} else {
+    document.write(' Entrou dentro do block if(falso)')
+}
+
+if ( 2 === '2') {
+    document.write(' Números iguais')
+} else {
+    document.write(' Números diferentes')
+}
+
+if ( 'b' != 'b') {
+    document.write(' valores diferentes')    
+} else {
+    document.write(' Valores iguais')
+}
+
+if ('b' !== 'a') {
+    document.write(' Não identicos')
+} else { 
+    document.write(' Identicos')
+}
+
+var hour = 20
+if (hour < 18) {
+    document.write(' Good day')
+} else {
+    document.write(' good nigth')
+}
+
+let dia = 9
+if (dia == 9) {
+    document.write(' é hoje')
+} else {
+    document.write('Talves amanhã')
+}
