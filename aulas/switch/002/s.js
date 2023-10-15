2e666ccf1b4a384cb7c2de663fa137245c195080
BernardoValle <dev.bernardovalle@gmail.com>
2023-10-15 19:49:53 -0300

    operadores

---
+++ aulas/switch/002/s.js
@@ -0,0 +1,14 @@
+var t = 2
+
+switch(t){
+    case 1:
+        document.write('Você digitou um')
+        break
+    
+    case 2:
+        document.write('Você digitou banana')
+        break
+    
+    default:
+        document.write('Default')
+}
